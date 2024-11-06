987b42e7203f4a9b79993fb5dc58733a6e2996b4
JhonathanGomez27 <jhonathango2741@gmail.com>
2024-11-06 08:35:25 -0500

    fix: Update footer text and URLs to use IP addresses instead of localhost

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
-    urlAuth: 'http://localhost:3000/monitoreo/authentication/',
-    url: 'http://localhost:3000/monitoreo/',
+    urlAuth: 'http://172.26.20.199:3000/monitoreo/authentication/',
+    url: 'http://172.26.20.199:3000/monitoreo/',
     pagination: 20,
     production: true
 };
